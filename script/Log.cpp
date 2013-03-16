7c014b9abeee41225f4768a729bab448343e2471
Jiang Youxin <jiangyouxin9@gmail.com>
2013-03-16 03:19:08 +0100

    add log

---
+++ script/Log.cpp
@@ -0,0 +1,13 @@
+#include "stdafx.h"
+#include "Log.h"
+
+void Log(const char *format, ...)
+{
+	va_list v;
+	va_start(v, format);
+
+	char buf[1024];
+	_vsnprintf_s(buf, 1024, format, v);
+
+	OutputDebugStringA(buf);
+}
---
+++ script/Log.h
@@ -0,0 +1,6 @@
+#ifndef LOG_H
+#define LOG_H
+
+void MyLog(const char *format, ...);
+
+#endif
